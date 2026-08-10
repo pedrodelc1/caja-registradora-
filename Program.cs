34551b3e57c462036e7bc6f14c0d975e8fa9d0e8
pedrodelc1 <pedrodelcarlo@icloud.com>
2026-08-10 10:15:06 -0300

    etapa 2: carga de producto

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
 ﻿Console.WriteLine("Hello, World!");
+const string nombreDelkiosco = "Kiosco EL RECREO";
+
+Console.WriteLine("nombre del cajero: ");
+string nombreCajero = Console.ReadLine();
+
+Console.WriteLine($"Bienvenida, {nombreCajero}. {nombreDelkiosco} tiene la caja abierta.");
+
+Console.WriteLine("Nombre del producto: ");
+string nombreProducto = Console.ReadLine();
+Console.WriteLine("Precio del producto: ");
+Decimal precio = Decimal.Parse(Console.ReadLine());
+Console.WriteLine($"Se creo el producto {nombreProducto}.");
+
+Console.ReadLine();
